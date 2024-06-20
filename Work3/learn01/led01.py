3a301442018c3f6641de50eb73972fcdaa238e8d
Lee Ju-hyi <dlwngml201@naver.com>
2024-06-20 16:27:59 +0900

    dfs

--- Work3/learn01/led01.py
+++ Work3/learn01/led01.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 led =21
 
 #GPIO를 BCM모드로 설정
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BGM)
 #GPIO 핀설정 (입력 /출력)
 GPIO.setup(led, GPIO.OUT)
 
 try:
 	while  True:
-		GPIO.output(led, False)
+		GPIO.outpin(led, False)
 
-except KeyboardInerrupt: #Ctnrl + c
+except KeyboardInterrupt: #Ctnrl + c
 	GPIO.cleanup()
--- Work3/learn01/led02.py
+++ Work3/learn01/led02.py
@@ -1,31 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
-red_pin =21
-green_pin =19
+# RGB LED Pinnumber setting
+red_pin = 21
+green_pin = 19
 blue_pin = 20
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(red_pin, GPIO.PUT)
-GPIO.setup(blue_pin, GPIO.PUT)
-GPIO.setup(green_pin, GPIO.PUT)
+GPIO.setmode(GPIO.BCM)  # GPIO.BOARD(1~40)
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(blue_pin, GPIO.OUT)
 
 try:
-	while True:
-		GPIO.output(red_pin, False)
-		GPIO.output(green_pin, True)
-		GPIO.output(blue_pin, True)
-		time.sleep(1)
+    while(True):
+        GPIO.output(red_pin, False)
+        GPIO.output(green_pin, True)
+        GPIO.output(blue_pin, True)
+        time.sleep(1)
 
-		GPIO.output(red_pin, True)
-    GPIO.output(green_pin, True)
-    GPIO.output(blue_pin, False)
-    time.sleep(1)
+        GPIO.output(red_pin, True)
+        GPIO.output(green_pin, True)
+        GPIO.output(blue_pin, False)
+        time.sleep(0.5)
 
-	  GPIO.output(red_pin, True)
-	  GPIO.output(green_pin, True)
-    GPIO.output(blue_pin, False)
-		time.sleep(1)
+        GPIO.output(red_pin, True)
+        GPIO.output(green_pin, False)
+        GPIO.output(blue_pin, True)
+        time.sleep(0.5)
 
-except keyboardInterrupt:
-	GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
